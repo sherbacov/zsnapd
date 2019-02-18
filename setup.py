79f1440bdb0e3071ed945829b141ede0e72e4899
Matthew Grant <matt@mattgrant.net.nz>
2019-02-18 19:28:45 +1300

    Release 0.8.8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from distutils.core import setup
 
 setup(name='zsnapd',
-    version='0.8.5',
+    version='0.8.8',
     description='ZFS Snapshot Daemon',
     author='Matthew Grant',
     author_email='matt@mattgrant.net.nz',
