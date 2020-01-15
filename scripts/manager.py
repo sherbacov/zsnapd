d8aafa160a032a6627d2723b86a250b1282d69b9
root <root@en-gedi.ad.anathoth.net>
2020-01-15 19:43:25 +1300

    Finish updating log messages

--- scripts/manager.py
+++ scripts/manager.py
@@ -263,7 +263,8 @@ class Manager(object):
                 snap_name = src_snapshots[snapshot]['name']
                 # There is a snapshot on this host that is not yet on the other side.
                 size = ZFS.get_size(src_dataset, prevsnap_name, snap_name, endpoint=src_endpoint, **extra_args)
-                log_info('[{0}] -   {1}@{2} > {1}@{3} ({4})'.format(local_dataset, src_dataset, prevsnap_name, snap_name, size))
+                log_info('[{0}] -   {1}@{2} > {3}@{4} ({5})'.format(local_dataset, src_dataset, prevsnap_name,
+                    dst_dataset, snap_name, size))
                 ZFS.replicate(src_dataset, prevsnap_name, snap_name, dst_dataset, replicate_settings['endpoint'],
                         direction=replicate_dirN, **extra_args)
                 Manager.new_hold(src_dataset, snap_name, endpoint=src_endpoint, log_command=log_command)
