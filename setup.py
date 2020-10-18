736abe2b9e34af6c1b52eb1c841b8189d320a374
Matthew Grant <matt@mattgrant.net.nz>
2020-10-19 11:13:09 +1300

    Bump version to 0.8.12

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@
 from distutils.core import setup
 
 setup(name='zsnapd',
-    version='0.8.10',
+    version='0.8.12',
     description='ZFS Snapshot Daemon',
     author='Matthew Grant',
     author_email='matt@mattgrant.net.nz',
